07f90cfe45ad9a96a9ced996e18407b4c335af6c
LiuPeng <tidycoder@gmail.com>
2016-01-26 20:20:38 +0800

    compile error

--- src/ios/CDVWXPhoto.m
+++ src/ios/CDVWXPhoto.m
@@ -8,23 +8,25 @@
 
 - (void)pick:(CDVInvokedUrlCommand *)command
 {
-    TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:1 delegate:self];
-    [self presentViewController:imagePickerVc animated:YES completion:nil];
+	// self.currentCallbackId = command.callbackId;
+	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.currentCallbackId];
+    // TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:1 delegate:self];
+    // [self presentViewController:imagePickerVc animated:YES completion:nil];
 }
 
 /// 用户点击了取消
-- (void)imagePickerControllerDidCancel:(TZImagePickerController *)picker {
-    // NSLog(@"cancel");
-	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.currentCallbackId];
-}
+// - (void)imagePickerControllerDidCancel:(TZImagePickerController *)picker {
+// 	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.currentCallbackId];
+// }
 
 /// 用户选择好了图片，如果assets非空，则用户选择了原图。
-- (void)imagePickerController:(TZImagePickerController *)picker didFinishPickingPhotos:(NSArray *)photos sourceAssets:(NSArray *)assets infos:(NSArray<NSDictionary *> *)infos{
-    NSString* url = [[infos firstObject] objectForKey:@"PHImageFileURLKey"];
-	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:url];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.currentCallbackId];
-}
+// - (void)imagePickerController:(TZImagePickerController *)picker didFinishPickingPhotos:(NSArray *)photos sourceAssets:(NSArray *)assets infos:(NSArray<NSDictionary *> *)infos{
+//     NSString* url = [[infos firstObject] objectForKey:@"PHImageFileURLKey"];
+// 	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:url];
+//     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.currentCallbackId];
+// }
 
 @end
 
