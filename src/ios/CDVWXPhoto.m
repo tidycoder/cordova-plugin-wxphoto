6452ac99fc2f85b52d7f1fa45f448bee45fbb83d
LiuPeng <tidycoder@gmail.com>
2016-01-27 11:34:56 +0800

    return dic

--- src/ios/CDVWXPhoto.m
+++ src/ios/CDVWXPhoto.m
@@ -9,10 +9,11 @@
 - (void)pick:(CDVInvokedUrlCommand *)command
 {
 	self.currentCallbackId = command.callbackId;
+	 __weak CDVCamera* weakSelf = self;
 	[self.commandDelegate runInBackground:^{
 	    TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:1 delegate:self];
 	    [self.viewController presentViewController:imagePickerVc animated:YES completion:nil];
-	}]
+	}];
 }
 
 /// 用户点击了取消
@@ -23,8 +24,9 @@
 
 /// 用户选择好了图片，如果assets非空，则用户选择了原图。
 - (void)imagePickerController:(TZImagePickerController *)picker didFinishPickingPhotos:(NSArray *)photos sourceAssets:(NSArray *)assets infos:(NSArray<NSDictionary *> *)infos{
-    NSString* url = [[infos firstObject] objectForKey:@"PHImageFileURLKey"];
-	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:url];
+    // NSString* url = [[infos firstObject] objectForKey:@"PHImageFileURLKey"];
+    NSDictionary* dic = [infos firstObject];
+	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dic];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.currentCallbackId];
 }
 
